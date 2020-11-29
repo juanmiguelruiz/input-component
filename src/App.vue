4a55405506b6b7efbe8bab94e3681c0e2f62ba1e
Juan Miguel <juanmiguelruizpazos@gmail.com>
2020-11-29 23:59:02 +0100

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <List/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import List from './components/List'
+import Footer from './components/Footer'
+
 export default {
   name: 'App',
   components: {
     List,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="center footer">juanmiguelruiz @ DevChallenges.io</p>
+    </footer>
+  );
+};
+
+
+export default Footer;
